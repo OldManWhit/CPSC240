eeacd85bec5f39cf03335b4471088bbbaf79f130
OldManWhit <112339527+OldManWhit@users.noreply.github.com>
2022-08-31 17:05:22 -0400

    Update Campus.java

--- Campus.java
+++ Campus.java
@@ -1,40 +1,23 @@
 import java.util.ArrayList;
 
 public class Campus {
-    static String campusName = null;
-    static ArrayList<String> campusPlace = new ArrayList<String>();
-    static String campusState = null;
+    private String campusName = null;
+    public ArrayList<String> campusPlace = new ArrayList<String>();
+    private static String campusState = null;
     
 
-
+    // parameterized constructor
+    public Campus(String campusName){
+        this.campusName = campusName;
+    }
     // Default constructor    
     public Campus() {
 
     }
 
-    // main
-    public static void main(String[] args) {
-        Campus UMW = new Campus();
-
-        campusState = "Virginia";
-        Campus.setCampusName("UMW");
-        campusPlace.add("green");
-
-        System.out.println(UMW.getCampusName());
-        System.out.println(UMW.getCampusPlace());
-        System.out.println(UMW.getCampusState());
-
-        Campus VSU = new Campus();
-        Campus.setCampusName("VSU");
-        campusPlace.add("blue");
-        System.out.println(VSU.getCampusName());
-        System.out.println(VSU.getCampusPlace());
-        System.out.println(VSU.getCampusState());
-        System.out.println(UMW.toString());
-    }
-
-    public void addPlaces(String place) {
+    public ArrayList<String> addPlaces(String place) {
         campusPlace.add(place);
+        return campusPlace;
     }
     /**
      * Getter for campusPlace
@@ -55,6 +38,7 @@ public class Campus {
      * @return campusState
      */
     public String getCampusState() {
+        campusState = "Virginia";
         return campusState;
     }
     /**
@@ -68,15 +52,14 @@ public class Campus {
      * Setter for campusName
      * @param name
      */
-    public static void setCampusName(String name){
+    public void setCampusName(String name){
         campusName = name;
-
     }
     /**
      * Setter for campusState
      * @param campusState passes
      */
-    public static void setCampusState() {
+    public void setCampusState() {
     //    Campus.campusState = campusState;
     }
     /**
