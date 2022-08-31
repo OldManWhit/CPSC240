03f2c6a27de562c0e5cf22753f75c42aa3ae77db
OldManWhit <112339527+OldManWhit@users.noreply.github.com>
2022-08-31 17:04:54 -0400

    Update CampusTester.java

--- CampusTester.java
+++ CampusTester.java
@@ -1,20 +1,28 @@
+import java.util.ArrayList;
+
 public class CampusTester {
 
-    public static void main(String[] args) {
-        String campusName = "UMW";
-        Campus campusUMW = new Campus(campusName,"Combs Hall");
-        System.out.println(campusUMW.toString());
-        System.out.println(campusUMW.getName() + " " + campusUMW.getPlace());
+     // main
+     public static void main(String[] args) {
+        Campus UMW = new Campus("UMW");
+        Campus VSU = new Campus("VSU");
+
+        UMW.addPlaces("Ball Circle");
+
+        ArrayList<String> whatever = new ArrayList<>();
+        whatever = UMW.campusPlace;
+
+        UMW.setCampusName("UMW");
+        campusPlace.add("green");
+
+        System.out.println(UMW.getCampusName());
+        System.out.println(UMW.getCampusPlace());
+        System.out.println(UMW.getCampusState());
+
+        Campus VSU = new Campus();
+        Campus.setCampusName("VSU");
+        campusPlace.add("blue");
+        System.out.println(VSU.getCampusName());
+
     }
-
-
-
-
-
-
-
-
-
-
-
 }
