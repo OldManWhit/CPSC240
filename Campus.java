d4b7d1e0730c8da9651a7a1f6f03d35f914ed128
OldManWhit <112339527+OldManWhit@users.noreply.github.com>
2022-08-31 13:01:04 -0400

    Update Campus.java

--- Campus.java
+++ Campus.java
@@ -1,60 +1,90 @@
-
-// Campus Class Defined
+import java.util.ArrayList;
 
 public class Campus {
-
-    private String campusName, campusPlace;
-
-
-    // Constructor 
-
-    public Campus(String campusName, String campusPlace) {
-        this.campusName = campusName;
-        this.campusPlace = campusPlace;
-    }
+    static String campusName = null;
+    static ArrayList<String> campusPlace = new ArrayList<String>();
+    static String campusState = null;
+    
 
 
-
-
+    // Default constructor    
     public Campus() {
 
     }
-    /**
-     * A getter to return Campus Name
-     * @return Name of the Campus
-     */
-    public String getName() {
-        return campusName;
+
+    // main
+    public static void main(String[] args) {
+        Campus UMW = new Campus();
+
+        campusState = "Virginia";
+        Campus.setCampusName("UMW");
+        campusPlace.add("green");
+
+        System.out.println(UMW.getCampusName());
+        System.out.println(UMW.getCampusPlace());
+        System.out.println(UMW.getCampusState());
+
+        Campus VSU = new Campus();
+        Campus.setCampusName("VSU");
+        campusPlace.add("blue");
+        System.out.println(VSU.getCampusName());
+        System.out.println(VSU.getCampusPlace());
+        System.out.println(VSU.getCampusState());
+        System.out.println(UMW.toString());
+    }
+
+    public void addPlaces(String place) {
+        campusPlace.add(place);
     }
     /**
-     * A getter to return the Campus Place
-     * @return Place on Campus
+     * Getter for campusPlace
+     * @return campusPlace
      */
-    public String getPlace() {
+    public ArrayList<String> getCampusPlace() {
         return campusPlace;
     }
     /**
-     * A setter to set the Campus Name
-     * @param cName sets the name of the Campus
+     * Getter for campusName
+     * @return campusName
      */
-    public void setName(String cName) {
-        campusName = cName;
+    public String getCampusName() {
+        return campusName;
+    }
+    /**
+     * Getter for campusState
+     * @return campusState
+     */
+    public String getCampusState() {
+        return campusState;
+    }
+    /**
+     * Setter for campusPlace ArrayList
+     * @param place sets the next place
+     */
+    public void setCampusPlace(ArrayList<String> place) {
+        campusPlace = place;
+    }
+    /**
+     * Setter for campusName
+     * @param name
+     */
+    public static void setCampusName(String name){
+        campusName = name;
 
     }
     /**
-     * A setter to set the Place on Campus
-     * @param cPlace sets the name of the Place on Campus
+     * Setter for campusState
+     * @param campusState passes
      */
-    public void setPlace(String cPlace) {
-        campusPlace = cPlace;
+    public static void setCampusState() {
+    //    Campus.campusState = campusState;
     }
-
-    //toString
-
+    /**
+     * toString() method to return the object created
+     */
     public String toString() {
         
-        return "The Campus name is " + campusName + " and the place to meet is at " + campusPlace + ".";
+        return "The Campus name is " + campusName + " and the place to meet is at " + campusPlace + ", in the state of" + campusState + ".";
 
     }
-
 }
